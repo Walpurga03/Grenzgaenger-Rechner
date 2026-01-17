58cb934ac1439ffcf76d53fac1678e2059a442d8
Walpurga03 <Walpurga03@gmx.at>
2026-01-17 18:26:41 +0100

    fix: Update base path and branch for GitHub Pages deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Grenzgänger-Rechner/',
+  base: '/Grenzgaenger-Rechner/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
